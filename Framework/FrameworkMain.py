959f257f265205ef693a3130bd02253590dcb723
Adrian <adrianfakstorp@gmail.com>
2020-02-18 16:00:22 -0800

    added text input field

---
+++ Framework/FrameworkMain.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# app.config['SECRET_KEY'] = "fb07659371ef163e955598bea0fecf44"
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/text-input")
+def forms():
+    return render_template('forms.html')
+
+@app.route("/text-input", methods=['POST'])
+def forms_post():
+    text = request.form['Input Your URL Here']
+    return text
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
